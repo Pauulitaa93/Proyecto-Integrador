9a7e4d2a16ab5f543c51836eb8a53790742b3e89
Emmanuel Villafranca <villafrancaemmanuel5@gmail.com>
2022-08-14 16:26:08 -0300

    EJ 1 y 2 python

--- ej_emmaVillafranca.py
+++ ej_emmaVillafranca.py
@@ -1,5 +1,6 @@
 import os
 os.system("cls")
+
 #punto 1
 dueno = [28957346,  "Juan",  "Perez", 4789689,  "Belgrano 101"]
 
@@ -14,20 +15,3 @@ for i in Dueno2:
     continue 
    print(i) 
 
-#Punto 3
-Perro = [13,  "Puppy",  "12/12/2012" , "Macho", 123]
-Perro[2] = "13/12/2012"
-Perro[4] = 28957346 
-print(Perro)
-
-#Punto 4
-Perro2 = [14,  "Fido",  "12/12/2012" , "Macho", 23546987]
-for i in reversed(Perro2):
-    print(i)
-#Punto 5
-Historial2 = (23500, 5960, 2300, 10200, 8900) 
-sumaPuppy = sum(Historial2)
-if sumaPuppy < 30000 :
-    print(sumaPuppy)
-else :
-    print("Gastos por encima de lo presupuestado")        
