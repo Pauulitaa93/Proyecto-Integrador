a0d5f9ca65a5f95f413a74c98d56dc3b438b8baf
Pauulitaa93 <107419500+Pauulitaa93@users.noreply.github.com>
2022-08-14 22:26:40 -0300

    Add files via upload
    Ejercicio 5

---
+++ Ejercicio_5.py
@@ -0,0 +1,6 @@
+historial = (2350, 5960, 23000, 1000, 8900)
+costo = sum(list(historial))
+if costo < 30000:
+    print ("Costo Total: ", costo)
+else:
+    print("Costos por encima de lo presupuestado")
